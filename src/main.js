ea55db28639a7315c137834e52d443270d48e3fa
ali abolfathi <ali.ab.99.202@gmail.com>
2022-02-16 12:11:26 +0330

    add source files & bootstrap & jquery & poper.js to project & link them to the project

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.rtl.min.css'
+import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './assets/css/style.css'
+import './assets/iconFont/css/fontello.css'
 
 createApp(App).use(store).use(router).mount('#app')
