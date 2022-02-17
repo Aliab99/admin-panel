42cb552d0fb437bd90363b8e0d4d38321f076527
amir monadi <amirmn8@gmail.com>
2022-02-17 17:32:35 +0330

    add prod component and product vue and edit index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Products from "../views/Products.vue";
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+
+  {
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/about',
-    name: 'About',
-    component:About
-  }
-]
+    path: "/products",
+    name: "Products",
+    component: Products,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
