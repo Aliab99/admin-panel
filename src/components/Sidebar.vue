d645f37a1f8cd62f2fa6d29f55c422e837e9b96a
ali abolfathi <ali.ab.99.202@gmail.com>
2022-02-17 18:24:32 +0330

    fix mistakes

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -19,41 +19,35 @@
 				<span class="icon-chart-pie-outline"></span>
 			</li>
 			<li class="my-1"><span class="icon-user-o"></span></li>
+			<li class="my-1"><span class="icon-basket"></span></li>
 			<li class="my-1">
-        <router-link to="/products"
-          ><span class="icon-basket"></span>
-        
-      </li>
-      <li class="my-1">
-        <router-link to="/products"
-          ><span class="icon-th-outline"></span>
-        </router-link>
-      </li>
+				<router-link class="link" to="/products"
+					><span class="icon-th-outline"></span>
+				</router-link>
+			</li>
 			<li class="my-1"><span class="icon-eye"></span></li>
 			<li class="my-1"><span class="icon-mail"></span></li>
 		</ul>
 	</aside>
-
 </template>
 
 <script>
-	export default  {
+	export default {
 		name: 'Sidebar',
 	};
 </script>
 
 <style scoped>
-
-  .router-link-exact-active{
-    padding: 10px;
-    border-radius: 5px;
-    background-color: #e0e0e0;
-    color : #292F45;
-  }
-  li:hover .link span{
-	  color : #01DEF8;
-  }
-  .link span{
-    color : #292F45;
-  }
+	.router-link-exact-active {
+		padding: 10px;
+		border-radius: 5px;
+		background-color: #e0e0e0;
+		color: #292f45;
+	}
+	li:hover .link span {
+		color: #01def8;
+	}
+	.link span {
+		color: #292f45;
+	}
 </style>
