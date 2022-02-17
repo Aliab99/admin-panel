3576bc75b60e2d9920b21aad1b9ba4f0239dbdbc
ali abolfathi <ali.ab.99.202@gmail.com>
2022-02-17 18:00:50 +0330

    create discount component and page and fix some style mistakes

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,12 +2,19 @@
 	<aside class="col-1 bg-white shadow-sm rounded d-none d-sm-block">
 		<p class="text-center my-3">Nikted Logo</p>
 		<ul class="m-0 p-0 d-flex flex-column align-items-center list-unstyled">
-			<!-- TODO:selected item must be showed by a gray rectangle -->
 			<li class="my-4">
-				<router-link to="/"><span class="icon-home-outline"></span></router-link>
+				<!-- home link -->
+				<router-link class="link" to="/">
+					<span class="icon-home-outline"></span>
+				</router-link>
 			</li>
 			<li class="mt-2 mb-4"><span class="icon-dot-3"></span></li>
-			<li class="my-1"><span class="icon-percent"></span></li>
+			<li class="my-1">
+				<!-- discount link -->
+				<router-link class="link" to="/DiscountCode">
+					<span class="icon-percent"></span>
+				</router-link>
+			</li>
 			<li class="my-1">
 				<span class="icon-chart-pie-outline"></span>
 			</li>
@@ -19,6 +26,13 @@
 		</ul>
 	</aside>
 </template>
+
+<script>
+	export default  {
+		name: 'Sidebar',
+	};
+</script>
+
 <style scoped>
   .router-link-exact-active{
     padding: 10px;
@@ -26,9 +40,10 @@
     background-color: #e0e0e0;
     color : #292F45;
   }
+  li:hover .link span{
+	  color : #01DEF8;
+  }
+  .link span{
+    color : #292F45;
+  }
 </style>
-<script>
-	export default {
-		name: 'Sidebar',
-	};
-</script>
---
+++ src/views/DiscountCode.vue
@@ -0,0 +1,22 @@
+<template>
+	<article class="row gx-3 ">
+		<section class="col-12 col-md-6 ">
+			<div class="p-3 shadow-sm bg-white rounded">Custom column padding</div>
+		</section>
+		<section class="col-12 col-md-6 ">
+			<DiscountGenerator/>
+		</section>
+	</article>
+</template>
+
+<script>
+	import DiscountGenerator from '../components/DiscountGenerator.vue';
+
+	export default {
+		name: 'Discount',
+		components: {
+			DiscountGenerator: DiscountGenerator,
+		},
+	};
+</script>
+
